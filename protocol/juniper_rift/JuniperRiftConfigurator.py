aaf3b069b947a37dfe5cccc47ee658fc5be3629c
Mariano Scazzariello <marianoscazzariello@gmail.com>
2021-04-08 15:15:27 +0200

    Switch to CRPD text config instead of gzip

--- protocol/juniper_rift/JuniperRiftConfigurator.py
+++ protocol/juniper_rift/JuniperRiftConfigurator.py
@@ -1,4 +1,3 @@
-import gzip
 import os
 
 from model.node_types.Leaf import Leaf
@@ -7,92 +6,47 @@ from ..IConfigurator import IConfigurator
 
 # --------------------------- Start of Juniper Rift configuration templates --------------------------------------------
 
-# the values for 
-# startup-holddown
-# lie-transmit-interval 
-# hold-time
-# are tuned for large topologies on a machine
 
 JUNIPER_RIFT_CONFIG_TEMPLATE = \
     """
-version "20201001.232355__devpublish.r3242 [_devpublish]";
-groups {
-    rift-defaults {
-        protocols {
-            rift {
-                traceoptions {
-                    file size 1000000 files 4;
-                    level notice;
-                }
-                node-id %s;
-                level %s;
-                lie-receive-address {
-                    family {
-                        inet 224.0.0.120;
-                    }
-                }
-                interface <*> {
-                    lie-transmit-address {
-                        family {
-                            inet 224.0.0.120;
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-system {
-    host-name %s;
-    root-authentication {
-        encrypted-password "$6$QMRUo$LNAfxi1P1RljyWHeUl6qFSDMzFAn8Kqzt.vo/LfcoAfwstTvc8qT0WUrnMMGhdW4Dn6p8V15nkP6/ySoHBIAP.";
-    }
-    scripts {
-        language python3;
-    }
-}
-
-protocols {
-    rift {
-        apply-groups rift-defaults;
-        name %s;
-        # startup-holddown 5;
+set groups rift-defaults protocols rift traceoptions file size 1000000
+set groups rift-defaults protocols rift traceoptions file files 4
+set groups rift-defaults protocols rift traceoptions level info
+set groups rift-defaults protocols rift node-id %s
+set groups rift-defaults protocols rift level %s
+set groups rift-defaults protocols rift lie-receive-address family inet 224.0.0.120
+set groups rift-defaults protocols rift lie-receive-address family inet6 ff02::a1f7
+set groups rift-defaults protocols rift interface <*> lie-transmit-address family inet 224.0.0.120
+set groups rift-defaults protocols rift interface <*> lie-transmit-address family inet6 ff02::a1f7
+set system host-name %s
+set system scripts language python3
+set system root-authentication encrypted-password "$6$QMRUo$LNAfxi1P1RljyWHeUl6qFSDMzFAn8Kqzt.vo/LfcoAfwstTvc8qT0WUrnMMGhdW4Dn6p8V15nkP6/ySoHBIAP."
+set protocols rift apply-groups rift-defaults
+set protocols rift name %s
+set protocols rift startup-holddown 30
 %s
 %s
-    }
-}
-
 %s
-"""
+    """
 
 JUNIPER_RIFT_CONFIG_INTERFACE_TEMPLATE = \
-    "        interface %s {\n" + \
-    "            # hold-time 600;\n" + \
-    "            # lie-transmit-interval 30;\n" + \
-    "        }"
+    """
+set protocols rift interface {iface}
+set protocols rift interface {iface} lie-transmit-interval 30
+set protocols rift interface {iface} hold-time 600
+    """
 
 JUNIPER_RIFT_EXPORT_TEMPLATE = \
-    "        export {\n" + \
-    "            northbound {\n" + \
-    "                directly;\n" + \
-    "            }\n" + \
-    "        }"
+    """
+set protocols rift export northbound directly
+    """
 
 JUNIPER_RIFT_POLICY_TEMPLATE = \
-    "policy-options {\n" + \
-    "    policy-statement directly {\n" + \
-    "        term t1 {\n" + \
-    "            from {\n" + \
-    "                protocol direct;\n" + \
-    "                route-filter %s/24 exact;\n" + \
-    "            }\n" + \
-    "            then accept;\n" + \
-    "        }\n" + \
-    "        term t2 {\n" + \
-    "            then reject;\n" + \
-    "        }\n" + \
-    "    }\n" + \
-    "}"
+    """
+set policy-options policy-statement directly term t1 from protocol direct route-filter %s/24 exact
+set policy-options policy-statement directly term t1 then accept
+set policy-options policy-statement directly term t2 then reject
+    """
 
 
 # --------------------------- End of Juniper Rift configuration templates ----------------------------------------------
@@ -105,9 +59,9 @@ class JuniperRiftConfigurator(IConfigurator):
 
         interfaces_strings = []
         for interface in node.get_phy_interfaces():
-            interfaces_strings.append(JUNIPER_RIFT_CONFIG_INTERFACE_TEMPLATE % interface.get_name())
+            interfaces_strings.append(JUNIPER_RIFT_CONFIG_INTERFACE_TEMPLATE.format(iface=interface.get_name()))
 
-        interfaces_string = "\n".join(interfaces_strings)
+        interfaces_string = "".join(interfaces_strings)
 
         if type(node) == Tof:
             node_id = "%d%d%d" % (node.plane, node.level, node.number)
@@ -123,14 +77,13 @@ class JuniperRiftConfigurator(IConfigurator):
             server_interface = list(filter(lambda eth: '/24' in str(eth.network), node.interfaces))
             policy_content = JUNIPER_RIFT_POLICY_TEMPLATE % str(server_interface[0].network.network_address)
 
-        config_path = "%s/%s/config" % (lab.lab_dir_name, node.name)
-        os.mkdir(config_path)
-        with gzip.open("%s/juniper_rift.conf.gz" % config_path, "wb") as config_file:
+        config_path = "%s/%s/etc" % (lab.lab_dir_name, node.name)
+        with open("%s/juniper_rift.conf" % config_path, "w") as config_file:
             config_content = JUNIPER_RIFT_CONFIG_TEMPLATE % (node_id, node_level, node.name,
                                                              node.name, export_content, interfaces_string,
                                                              policy_content)
 
-            config_file.write(config_content.encode('utf-8'))
+            config_file.write(config_content)
 
         for service in ['auditd', 'eventd', 'license-check', 'na-grpcd', 'ppmd', 'xmlproxyd', 'bfdd', 'jsd', 'mgd-api',
                         'na-mqttd', 'rsyslog']:
@@ -143,5 +96,6 @@ class JuniperRiftConfigurator(IConfigurator):
             startup.write(
                 "\nfor d in rpd mgd rift-proxyd; do until pidof $d; do sleep 1; done; done\n" +
                 "until cli -c \"show version\" | grep -i junos; do sleep 1; done\n" +
-                "cli -c \"configure; load replace /config/juniper_rift.conf.gz; commit\" &> /tmp/config.errors"
+                "until [ -f /config/juniper.conf.gz ]; do sleep 1; done\n" +
+                "cli -c \"configure; load set /etc/juniper_rift.conf; commit\" &> /tmp/config.errors"
             )
